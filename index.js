aab66d024de2c497bf155687c68ca63cc20de44c
Elizabeth Maria <e@elm.sh>
2019-01-06 23:05:44 +0000

    feat: add meta tag parsing

--- index.js
+++ index.js
@@ -4,6 +4,48 @@ const JSDOM = require("jsdom").JSDOM;
 
 const { PORT=3000 } = process.env;
 
+// takes in a DOM object, and returns as many opengraph properties as we can
+readOpengraph = (doc) => {
+	let propsMap = {
+		title: "meta[property='og:title']",
+		type: "meta[property='og:type']",
+		url: "meta[property='og:url']",
+		image: "meta[property='og:image']",
+		description: "meta[property='og:description']",
+		siteName: "meta[property='og:site_name']",
+
+		published: "meta[property='article:published_time']",
+		modified: "meta[property='article:modified_time']",
+		author: "meta[property='article:author']",
+	};
+
+	let propsMapAll = {
+		tag: "meta[property='article:tag']",
+	};
+
+	let ret = {};
+
+	Object.keys(propsMap).forEach((key) => {
+		let selected = doc.querySelector(propsMap[key]);
+
+		if (selected)
+			ret[key] = selected.content;
+	});
+
+	Object.keys(propsMapAll).forEach((key) => {
+		let selected = doc.querySelectorAll(propsMapAll[key]);
+
+		if (selected) {
+			ret[key] = [];
+
+			for (let i = 0; i < selected.length; i++)
+				ret[key].push(selected[i].content);
+		}
+	});
+
+	return ret;
+};
+
 polka()
 	.post("/api/get", (req, res) => {
 		let body = "";
@@ -26,9 +68,17 @@ polka()
 				let reader = new Readability(doc.window.document);
 				let article = reader.parse();
 
-				res.end(JSON.stringify(article));
+				let meta = readOpengraph(doc.window.document);
+
+				res.end(JSON.stringify({
+					// this is just taken from the HTML by readability
+					article: article,
+					// this is parsed from the meta tags, and will be more accurate if it is there.
+					meta: meta,
+				}));
 
 			} catch(e) {
+				console.log(e);
 				res.statusCode = 400;
 				res.end("Bad Request");
 			}
